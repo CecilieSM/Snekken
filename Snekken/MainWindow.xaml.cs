69c99a69637f7f3477fc083729b5091ad2d83bf9
Wuway <contactdieter@gmail.com>
2025-10-31 13:10:29 +0100

    Oprettet views til: - MainWindow - OpretRessource - OpretRessourceType

--- Snekken/MainWindow.xaml.cs
+++ Snekken/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Snekken.View;
 
 namespace Snekken
 {
@@ -19,6 +20,14 @@ namespace Snekken
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void RessourcerButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreateRessource objRessourceWindow = new CreateRessource();
+            this.Visibility = Visibility.Hidden;
+            objRessourceWindow.Show();
         }
     }
 }
---
+++ Snekken/View/CreateRessourceType.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Snekken.View
+{
+    /// <summary>
+    /// Interaction logic for CreateRessourceType.xaml
+    /// </summary>
+    public partial class CreateRessourceType : Window
+    {
+        public CreateRessourceType()
+        {
+            InitializeComponent();
+        }
+    }
+}
