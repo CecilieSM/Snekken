f3857ce0160befb80c697d9eb511430399632b21
LauraPowerHansen <laha71386@edu.ucl.dk>
2025-11-06 10:31:37 +0100

    Model klasser - Resource, Person, Booking

---
+++ Models/Booking.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models;
+
+public class Booking
+{
+    // PK
+    public int Id { get; set; }
+
+    // Tidsrum for booking
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
+    // Statusser 
+    public bool RequirementStatus { get; set; }  // opfyldt/ikke opfyldt
+    public bool PaymentStatus { get; set; }      // depositum betalt/ikke betalt
+
+    // FK’er
+    public int ResourceId { get; set; }         
+    public int PersonId { get; set; }           
+
+    public Booking() 
+    { 
+    }
+
+    public Booking(int resourceId, int personId, DateTime startTime, DateTime endTime, bool requirementStatus = false, bool paymentStatus = false)
+    {
+        ResourceId = resourceId;
+        PersonId = personId;
+        StartTime = startTime;
+        EndTime = endTime;
+        RequirementStatus = requirementStatus;
+        PaymentStatus = paymentStatus;
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models;
+public class Person
+{
+    // PK
+    public int Id { get; set; }
+
+    // Data
+    public string Name { get; set; } = "";
+    public string? Mobile { get; set; }          // string, ikke int pga. telefonnummer 
+    public string Email { get; set; } = "";
+
+    public Person() { }
+
+    public Person(string name, string email, string? mobile = null)
+    {
+        Name = name;
+        Email = email;
+        Mobile = mobile;
+    }
+}
