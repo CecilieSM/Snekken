cec03cc2eca1e4777c6af7f07f5b90aeaaacb58a
Cecilie <ceciliek1989@outlook.com>
2025-11-10 13:21:12 +0100

    save before switching branches

---
+++ Models/Schema/DummyData.sql
@@ -0,0 +1,6 @@
+﻿--Create Resource, ResourceType, Person, Booking
+--INSERT INTO Resource(title, price, isActive, resourceType) VALUES('Jolle', 550, true, 'båd')
+--INSERT INTO ResourceType(title, timeUnit, status, requirement) VALUES('båd', 2, true)
+--INSERT INTO Person(name, phone, email) VALUES('Peter Hansen', 56614525, 'peter.hansen@gmail.com')
+--INSERT INTO Booking(startTime, endTime, requirementFulfilled, isPaid) VALUES(2026-07-16 10:00, 2026-07-16 12:00, true, false)
+
