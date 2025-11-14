fe1e3cbca301d6dfb69ba58b23496e38f077b143
Dieter Knockaert <contactdieter@gmail.com>
2025-11-14 14:00:40 +0100

    added binding VM View

--- Snekken/ViewModel/ResourceViewModel.cs
+++ Snekken/ViewModel/ResourceViewModel.cs
@@ -59,8 +59,11 @@ public class ResourceViewModel : BaseViewModel
         }
     }
 
+    public Array TimeUnitOptions => Enum.GetValues(typeof(TimeUnit));
+
     private string? _typeFormRequirement;
     public string? TypeFormRequirement { get => _typeFormRequirement; set { _typeFormRequirement = value; OnPropertyChanged(); } }
+
 
 
     ////PROPERTIES PERSON
