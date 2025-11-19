3b5fa353d519934e3df672c01b43dfa9bd513ab3
Dieter Knockaert <contactdieter@gmail.com>
2025-11-19 15:20:41 +0100

    Moved IMessageService and MessageService to Services folder in Snekken because MessageBox.Show() needs to be a part of a WPF project (snekken)

---
+++ Snekken/Services/MessageService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Models.Repository
+{
+    public class MessageService
+    {
+        public void Show(string message)
+        {
+            // Real UI dialog (WPF / WinForms)
+            MessageBox.Show(message);
+        }
+    }
+}
