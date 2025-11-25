10b33d2f304ab14ebe7849cc09fc77c6fcdd0b6f
Wuway <contactdieter@gmail.com>
2025-11-25 15:33:35 +0100

    Opret RentalKiosk Opret WPFLib project

--- RentalKiosk/App.xaml.cs
+++ RentalKiosk/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using WPFLib.Services;
+using RentalKiosk.ViewModels;
 
 namespace RentalKiosk
 {
@@ -9,6 +11,28 @@ namespace RentalKiosk
     /// </summary>
     public partial class App : Application
     {
+        //    private IMessageService _messageService;
+        //    private MainViewModel _mainViewModel;
+
+        //    protected override void OnStartup(StartupEventArgs e)
+        //    {
+        //        base.OnStartup(e);
+
+        //        //Create services
+        //        _messageService = new MessageService();
+
+        //        // Create ViewModels (injecting services manually)
+        //        _mainViewModel = new MainViewModel(_messageService);
+
+        //        // Create and show MainWindow
+        //        MainWindow window = new MainWindow
+        //        {
+        //            DataContext = _mainViewModel
+        //        };
+
+        //        window.Show();
+        //    }
+        }
     }
 
-}
+
---
+++ WPFLib/Services/MessageService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace WPFLib.Services;
+
+public static class MessageService
+{
+    public static void Show(string message)
+    {
+        MessageBox.Show(message);
+    }
+
+    public static void Log(string message) 
+    {
+        string path = "log.txt";
+
+        if (!File.Exists(path)) 
+        {
+            File.Create(path).Dispose();
+        }
+
+        using (StreamWriter writer = new StreamWriter(path, append: true))
+        {
+            writer.WriteLine(message);
+        }
+    }
+}
+
