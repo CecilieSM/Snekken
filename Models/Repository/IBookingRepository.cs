975bbcf66c5727224fd7d41d1b1d92987848c819
Cecilie <ceciliek1989@outlook.com>
2025-11-27 09:13:16 +0100

    save before switching branches

---
+++ Models/Repository/IBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Repository;
+
+public interface IBookingRepository : IRepository<Booking>
+{
+    IEnumerable<Booking> GetByResourceId(int resourceId);
+    IEnumerable<Booking> GetByPersonId(int personId);
+}
