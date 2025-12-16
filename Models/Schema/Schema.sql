3af4ff919bc5d1c5f5799f35a1066fef6689379b
Cecilie <ceciliek1989@outlook.com>
2025-12-16 13:53:01 +0100

    db

--- Models/Schema/Schema.sql
+++ Models/Schema/Schema.sql
@@ -38,24 +38,24 @@ CREATE TABLE BOOKING (
 );
 
 
-GO
+--GO
 
-CREATE TRIGGER PreventOverlap
-ON BOOKING
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM BOOKING b
-        JOIN inserted i
-          ON b.ResourceId = i.ResourceId
-         AND b.StartTime < i.EndTime
-         AND b.EndTime > i.StartTime
-         AND b.BookingId <> i.BookingId   -- avoid self‑match on update
-    )
-    BEGIN
-        RAISERROR ('Booking overlaps with existing booking for this resource', 16, 1);
-        ROLLBACK TRANSACTION;
-    END
-END;
+--CREATE TRIGGER PreventOverlap
+--ON BOOKING
+--AFTER INSERT, UPDATE
+--AS
+--BEGIN
+--    IF EXISTS (
+--        SELECT 1
+--        FROM BOOKING b
+--        JOIN inserted i
+--          ON b.ResourceId = i.ResourceId
+--         AND b.StartTime < i.EndTime
+--         AND b.EndTime > i.StartTime
+--         AND b.BookingId <> i.BookingId   -- avoid self‑match on update
+--    )
+--    BEGIN
+--        RAISERROR ('Booking overlaps with existing booking for this resource', 16, 1);
+--        ROLLBACK TRANSACTION;
+--    END
+--END;
